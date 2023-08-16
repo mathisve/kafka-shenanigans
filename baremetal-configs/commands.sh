8fdf2b5ca55131c20c6c57c437b3063543899ed6
Mathis Van Eetvelde <mathis.vaneetvelde@protonmail.com>
2023-08-16 16:20:17 -0600

    Update to kraft

--- baremetal-configs/commands.sh
+++ baremetal-configs/commands.sh
@@ -4,29 +4,8 @@
 # JAVA #
 ########
 
-sudo apt-get install openjdk-11-jre-headless
-
-
-#############
-# ZOOKEEPER #
-#############
-
-wget https://dlcdn.apache.org/zookeeper/zookeeper-3.9.0/apache-zookeeper-3.9.0-bin.tar.gz\
-sudo mkdir /usr/local/zookeeper
-sudo chown -R $(whoami) /usr/local/zookeeper
-sudo tar \ 
-    -xzf apache-zookeeper-3.9.0-bin.tar.gz \
-    -C /usr/local/zookeeper/ \
-    --strip-components=1
-
-# zookeeper config
-echo "tickTime=2000
-dataDir=/var/lib/zookeeper
-clientPort=2181 " >> /usr/local/zookeeper/conf/zoo.cfg
-
-# start zookeeper
-sudo /usr/local/zookeeper/bin/zkServer.sh start
-
+sudo apt update
+sudo apt install openjdk-11-jre-headless -y
 
 #########
 # KAFKA #
@@ -34,15 +13,24 @@ sudo /usr/local/zookeeper/bin/zkServer.sh start
 
 wget https://downloads.apache.org/kafka/3.5.1/kafka_2.13-3.5.1.tgz
 sudo mkdir /usr/local/kafka
-sudo tar \
-    -xzf kafka_2.13-3.5.1.tgz \ 
-    -C /usr/local/kafka \
-    --strip-components=1
-
 sudo chown -R $(whoami) /usr/local/kafka
 
+tar \
+	-xzf kafka_2.13-3.5.1.tgz \
+	-C /usr/local/kafka \
+	--strip-components=1
+
+# set up kraft
+export uuid=$(/usr/local/kafka/bin/kafka-storage.sh random-uuid)
+
+/usr/local/kafka/bin/kafka-storage.sh format \
+	-t $uuid \
+	-c /usr/local/kafka/config/kraft/server.properties
+
 # start kafka
-/usr/local/kafka/bin/kafka-server-start.sh -daemon /usr/local/kafka/config/server.properties
+/usr/local/kafka/bin/kafka-server-start.sh \
+	-daemon \
+	/usr/local/kafka/config/kraft/server.properties
 
 
 ###############
@@ -55,6 +43,12 @@ sudo chown -R $(whoami) /usr/local/kafka
     --bootstrap-server localhost:9092 \
     --partitions 10
 
+/usr/local/kafka/bin/kafka-topics.sh \
+	--create \
+	--topic deadletter \
+	--bootstrap-server localhost:9092 \
+	--partitions 10
+
 
 #################
 # KAFKA CONNECT #
@@ -66,7 +60,7 @@ echo "plugin.path=/usr/local/kafka/plugins" >> /usr/local/kafka/config/connect-d
 # download connector
 wget https://repo.maven.apache.org/maven2/org/apache/camel/kafkaconnector/camel-postgresql-sink-kafka-connector/3.18.2/camel-postgresql-sink-kafka-connector-3.18.2-package.tar.gz
 
-sudo tar \
+tar \
     -xzf camel-postgresql-sink-kafka-connector-3.18.2-package.tar.gz \
     -C /usr/local/kafka/plugins/camel-postgresql-sink-kafka-connector \
     --strip-components=1
@@ -77,8 +71,19 @@ mv postgresql-42.6.0.jar /usr/local/kafka/plugins/camel-postgresql-sink-kafka-co
 
 # start kafka connect
 /usr/local/kafka/bin/connect-distributed.sh \
-    -daemon \
+	-daemon \
     /usr/local/kafka/config/connect-distributed.properties
+
+# loop that waits for kafka connect to come online
+# only useful when this file is run as a whole
+# disregard when manually executing commands
+while true; do
+	curl -X GET http://localhost:8083 < /dev/null
+	if [ $? -eq 0 ]; then
+		break
+	fi
+	sleep 15
+done
 
 # timescale-sink config
 echo '{
@@ -93,8 +98,8 @@ echo '{
 		"topics":"mytopic",
 		"camel.kamelet.postgresql-sink.databaseName":"tsdb",
 		"camel.kamelet.postgresql-sink.password":"password",
-		"camel.kamelet.postgresql-sink.query":"INSERT INTO accounts (username,city) VALUES (:#username,:#city)",
-		"camel.kamelet.postgresql-sink.serverName":"service.project.tsdb.cloud.timescale.com",
+		"camel.kamelet.postgresql-sink.query":"INSERT INTO accounts (name,city) VALUES (:#name,:#city)",
+		"camel.kamelet.postgresql-sink.serverName":"service_id.project_id.tsdb.cloud.timescale.com",
 		"camel.kamelet.postgresql-sink.serverPort":"5432",
 		"camel.kamelet.postgresql-sink.username":"tsdbadmin"
 	}
@@ -104,14 +109,16 @@ echo '{
 cat timescale-sink.properties | curl -X POST -d @- http://localhost:8083/connectors -H "Content-Type: application/json"
 
 # validate if connector shows up
-curl -X GET https://localhost:8083/connectors
+curl -X GET http://localhost:8083/connectors
 
 
 ############
 # KAFKACAT #
 ############
 
-sudo apt-get install kafkacat
+sudo apt install kafkacat -y
 
 #send message on topic
-echo '{"username":"Mathis","city":"Salt Lake City"}' | kafkacat -P -b localhost:9092 -t mytopic
+echo '{"name":"Mathis","city":"Salt Lake City"}' | kafkacat -P -b localhost:9092 -t mytopic
+echo '{"name":"Oliver","city":"Moab"}' | kafkacat -P -b localhost:9092 -t mytopic
+echo '{"name":"Mark","city":"Park City"}' | kafkacat -P -b localhost:9092 -t mytopic
